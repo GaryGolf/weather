3e513eae7f0792659527ee8d562ad46b1dcf0cff
GaryGolf <garygolf@mail.ru>
2017-10-05 18:29:13 +0300

    add simple navigation

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,27 @@
 import * as React from 'react'
-import { Provider, Store } from 'react-redux';
+import { Provider, Store } from 'react-redux'
+import { StackNavigator, DrawerNavigator } from 'react-navigation'
 import store from './store'
 import Main from './screens/main'
 
 interface Props {}
 interface State {}
 
+const Stack = {
+  Main: { screen: Main}
+}
+
+const Drawer = {
+  MainStack: {name: 'MainStack', screen: StackNavigator(Stack, {initialRouteName: 'Main'})}
+}
+
+const Router = StackNavigator({Drawer:{name:'Drawer',screen:DrawerNavigator(Drawer)},...Stack},{headerMode: 'none'})
+
 export default class TaxiApp extends React.Component<Props,State>{
   render() {
     return (
       <Provider store={store}>
-        <Main/>
+        <Router/>
       </Provider>
     )
   }
--- src/screens/main.tsx
+++ src/screens/main.tsx
@@ -1,10 +1,36 @@
 import * as React from 'react'
-import { View, Text, StyleSheet} from 'react-native'
+import { View, Text, StyleSheet, TouchableHighlight} from 'react-native'
+import { NavigationParams, NavigationStackScreenOptions } from 'react-navigation'
+import Options from '../components/media/options'
+import Menu from '../components/media/menu'
 
-interface Props {}
+interface Props {
+  navigation?: NavigationParams
+}
 interface State {}
 
 export default class Main extends React.Component <Props, State> {
+  static navigationOptions = ({navigation}) => ({
+    title: 'Weather',
+    headerLeft: (
+        <TouchableHighlight onPress={()=>navigation.navigate('DrawerOpen')}> 
+             <View style={styles.menu}>
+                {/* <Menu/> */}
+                <Text>{'<'}</Text>
+            </View> 
+        </TouchableHighlight>
+    ),
+    headerRight: (
+        <TouchableHighlight onPress={()=>navigation.navigate('DrawerOpen')}> 
+             <View style={styles.menu}>
+                {/* <Options/> */}
+                <Text>{'*'}</Text>
+            </View> 
+        </TouchableHighlight>
+    ),
+    headerStyle: {backgroundColor: '#512da7'} ,
+    headerTitleStyle: { color: '#fff'}
+}) 
   render(){
     return (
       <View style={styles.container}>
@@ -20,5 +46,9 @@ const styles = StyleSheet.create({
       width: '100%',
       justifyContent: 'center',
       alignItems: 'center',
-  }
+  },
+  menu: {
+    margin: 10,
+    padding: 4
+}
 })
