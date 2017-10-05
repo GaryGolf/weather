47aa5bb68ee866f67c660175f7fd1bcecb475a7d
GaryGolf <garygolf@mail.ru>
2017-10-05 17:48:20 +0300

    add url constants

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,1 +1,5 @@
 export const PRODUCTION = false
+
+// url
+export const yahooWeatherURL = (city:string, U:string) => `https://query.yahooapis.com/v1/public/yql?q=select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="${city}") and u="${U}"&format=json`
+export const yahooLabelURL = 'https://poweredby.yahoo.com/purple.png'
--- src/store.ts
+++ src/store.ts
@@ -1,12 +1,12 @@
 import { createStore, applyMiddleware, Store, Middleware } from 'redux'
 import {createLogger} from 'redux-logger'
 import ReduxThunk from 'redux-thunk'
-import ReduxPromise from 'redux-promise-middleware'
+import ReduxPromise from 'redux-promise'
 import rootReducer, { RootState } from './reducers/index'
 import { PRODUCTION } from './constants/index'
 
 function configureStore(initialState?: RootState): Store<RootState> {
-    const middleware: Middleware[] = [ReduxThunk, ReduxPromise() ]
+    const middleware: Middleware[] = [ReduxThunk, ReduxPromise ]
     if(!PRODUCTION) middleware.push(createLogger({collapsed: true}))  
     const createStoreWithMiddleware = applyMiddleware(...middleware)(createStore)
     return createStoreWithMiddleware(rootReducer, initialState) as Store<RootState>
